679b2fa4e738668c7f51911a7a83a9167411d01f
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2025-09-03 18:17:01 +0800

    feat(db): add reading and story status values

---
+++ drizzle/0001_round_purifiers.sql
@@ -0,0 +1,3 @@
+ALTER TYPE "public"."reading_status" ADD VALUE 'PlanToRead';--> statement-breakpoint
+ALTER TYPE "public"."reading_status" ADD VALUE 'DroppedAsAbandoned';--> statement-breakpoint
+ALTER TYPE "public"."story_status" ADD VALUE 'Unknown';
