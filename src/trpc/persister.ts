16c52674882b5687bd6818a5baf1039744a3e85d
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2025-09-03 15:40:04 +0800

    feat(trpc): implement async storage persister with IndexedDB

--- src/trpc/persister.ts
+++ src/trpc/persister.ts
@@ -1,12 +1,22 @@
 import { createAsyncStoragePersister } from "@tanstack/query-async-storage-persister";
 import type { Persister } from "@tanstack/react-query-persist-client";
+import { createStore, del, get, set } from "idb-keyval";
+import SuperJSON from "superjson";
+
+const store = createStore("topaz", "cache");
 
 let persisterSingleton: Persister;
 export const getPersister = () => {
     if (typeof window !== "undefined") {
         persisterSingleton = createAsyncStoragePersister({
-            // TODO: Move to IndexedDB, but currently using localStorage will suffice
-            storage: window.localStorage,
+            key: "TOPAZ_CACHE",
+            storage: {
+                getItem: (key) => get(key, store),
+                setItem: (key, value) => set(key, value, store),
+                removeItem: (key) => del(key, store),
+            },
+            serialize: (client) => SuperJSON.stringify(client),
+            deserialize: (cachedString) => SuperJSON.parse(cachedString),
         });
     }
     return persisterSingleton;
