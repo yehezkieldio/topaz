532a94a56938db499dff3c8ebfbab4b7f663f7e2
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2025-09-28 10:03:49 +0800

    feat: remove library

--- src/app/(main)/library/page.tsx
+++ src/app/(main)/library/page.tsx
@@ -1,10 +1,7 @@
-import { AuthUserGate } from "#/components/auth-user-gate";
-import { LibraryClientProvider } from "#/features/library/components/list/library-client";
-
 export default function Library() {
     return (
-        <AuthUserGate>
-            {(isAdministratorUser) => <LibraryClientProvider isAdministratorUser={isAdministratorUser} />}
-        </AuthUserGate>
+        <div>
+            <h1>Hello, world!</h1>
+        </div>
     );
 }
