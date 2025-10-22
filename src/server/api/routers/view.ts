451c564c837ec4148ed180a570f2d507cb0c2960
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2025-10-22 11:06:44 +0800

    refactor(api): conditionally log materialized view refreshes

--- src/server/api/routers/view.ts
+++ src/server/api/routers/view.ts
@@ -1,3 +1,4 @@
+import { isDevelopment } from "#/env";
 import { createTRPCRouter, protectedProcedure, publicProcedure } from "#/server/api/trpc";
 import { libraryMaterializedView, libraryStatsMaterializedView } from "#/server/db/schema/view";
 
@@ -5,19 +6,25 @@ export const viewRouter = createTRPCRouter({
     refreshAll: protectedProcedure.mutation(async ({ ctx }) => {
         await ctx.db.refreshMaterializedView(libraryMaterializedView).concurrently();
         await ctx.db.refreshMaterializedView(libraryStatsMaterializedView);
-        console.log("[trpc] Refreshed user library materialized view");
+        if (isDevelopment === false) {
+            console.log("[trpc] Refreshed all materialized views");
+        }
 
         return { success: true };
     }),
     refreshLibrary: protectedProcedure.mutation(async ({ ctx }) => {
         await ctx.db.refreshMaterializedView(libraryMaterializedView).concurrently();
-        console.log("[trpc] Refreshed user library materialized view");
+        if (isDevelopment === false) {
+            console.log("[trpc] Refreshed user library materialized view");
+        }
 
         return { success: true };
     }),
     refreshLibraryStats: protectedProcedure.mutation(async ({ ctx }) => {
         await ctx.db.refreshMaterializedView(libraryStatsMaterializedView);
-        console.log("[trpc] Refreshed user library stats materialized view");
+        if (isDevelopment === false) {
+            console.log("[trpc] Refreshed user library stats materialized view");
+        }
 
         return { success: true };
     }),
